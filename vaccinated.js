ccbba887f7910857ecb2b84ab271ec179aaef095
aravindnan <52107115+aravindnan@users.noreply.github.com>
2021-02-16 18:49:58 +0530

    Add files via upload

---
+++ vaccinated.js
@@ -0,0 +1,17 @@
+function vac1(){
+    document.getElementById("vac1").innerHTML=
+    "Cipla has launched a generic vaccine for COVID 19.Remdesivir is the only drug approved by United States Food and Drug Administration (USFDA) for Emergency Use Authorisation (EUA) treatment of adult and paediatric patients hospitalised with suspected or laboratory confirmed COVID-19 infection.For more info click vaccine name"
+}
+
+function vac2(){
+    document.getElementById("vac2").innerHTML=
+    "The Pfizer-BioNTech COVID-19 vaccine has not been approved or licensed by the U.S. Food and Drug Administration (FDA), but has been    authorized for emergency use by FDA under an    Emergency Use Authorization (EUA) to prevent     Coronavirus Disease 2019 (COVID-19) for use in    individuals 16 years of age and older.For more info click vaccine name"
+}
+
+function vac3(){
+    document.getElementById("vac3").innerHTML=
+    "India's indigenous COVID-19 vaccine by Bharat Biotech is developed in collaboration with the Indian Council of Medical Research (ICMR) National Institute of Virology (NIV).The indigenous,inactivated vaccine is developed and manufactured in Bharat Biotech's BSL-3 (Bio-Safety Level 3) high  containment facility.For more info click vaccine name"
+}
+
+
+
